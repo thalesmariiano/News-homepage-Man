a7de51209636c7c78809cd990f0466a9a6240b6b
Thales Mariano <thales.mariano125@gmail.com>
2023-05-01 21:06:52 -0300

    colors adjusted to tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ['./src/**/*.{html, js}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'd-grayish-blue': '#5d5f79',
+        'grayish-blue': '#00001a',
+        'soft-red': '#f15e50',
+        'soft-orange': '#e9ab53'
+      }
+    },
     fontFamily: {
       inter: ['Inter', 'sans-serif']
     }
